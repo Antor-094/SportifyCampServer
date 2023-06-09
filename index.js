800fc51cf5d813d66b3ac417284600bb12409a4f
Antor-094 <mdantor556677889@gmail.com>
2023-06-09 12:20:06 +0600

    Selected Course post operation added

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
 
 
 const app = express();
 // app.use(cors());
 app.use(cors())
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 
@@ -30,8 +48,12 @@ async function run() {
 
     const courseCollections = client.db("campSporty").collection("courses");
     const instructorCollection= client.db("campSporty").collection("instructors");
-
-
+const selectedCoursesCollection= client.db('campSporty').collection('selectedCourses')
+   app.post('/jwt',(req,res)=>{
+    const user = req.body
+    const token = jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+    res.send({token})
+   })
 
 //all Courses get operation
 
@@ -46,8 +68,17 @@ app.get('/courses', async (req, res) => {
     const result = await instructorCollection.find().toArray();
     res.send(result);
   })
+  app.get('/selectedcourse', async (req, res) => {
+    
+    const result = await selectedCoursesCollection.find().toArray();
+    res.send(result);
+  })
 
-
+app.post('/selectedcourse',async (req,res)=>{
+  const body = req.body
+  const result = await selectedCoursesCollection.insertOne(body)
+  res.send(result)
+})
 
 
 
