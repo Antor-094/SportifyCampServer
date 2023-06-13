5a5b0589b81363c047ec93e192cca59e2f68680f
Antor-094 <mdantor556677889@gmail.com>
2023-06-13 18:45:06 +0600

    Instructor get operation done

--- index.js
+++ index.js
@@ -107,6 +107,19 @@ async function run() {
       res.send(result);
 
     })
+
+
+    app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ instructor: false })
+      }
+      const query = { email: email }
+      const user = await usersCollections.findOne(query);
+      const result = { instructor: user?.role === 'instructor' }
+      res.send(result);
+    })
     app.patch('/users/instructor/:id', async (req, res) => {
       const id = req.params.id;
       console.log(id);
